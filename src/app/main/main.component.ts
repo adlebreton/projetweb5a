78267376d8fb8d587e09bb4e95d2c47fdbb8df4d
eliott <eliott.denez@laposte.net>
2021-01-08 14:01:49 +0100

    V1 ajout et supp objectif

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Objectif} from './objectif';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  id: number | undefined;
+  name: string | undefined;
+  objList: Array<Objectif> =[{id:0, name:"essai0"},{id:1,name:"essai1"},{id:2,name:"essai2"}];
+  nb:number=0;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  Add(){
+    let objObj = new Objectif();
+    objObj.id=this.nb;
+    objObj.name="TEST";
+    this.objList.push(objObj);
+    this.name="";
+    this.id=0;
+    this.nb+=1;
+
+  }
+
+  Supp(i:number)
+  {
+    this.objList.splice(i,1);
   }
 
 }
---
+++ src/app/main/objectif.ts
@@ -0,0 +1,12 @@
+import { identifierModuleUrl } from "@angular/compiler";
+
+export class Objectif {
+  
+    id: number | undefined;
+    name: string | undefined;
+
+  
+    
+  }
+
+  
